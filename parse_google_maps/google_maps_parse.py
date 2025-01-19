3033b17dfe4718f08881af8d305fb8648a727763
russest3 <s.p.russell00@gmail.com>
2025-01-19 13:30:22 -0500

    updates

--- parse_google_maps/google_maps_parse.py
+++ parse_google_maps/google_maps_parse.py
@@ -6,30 +6,31 @@ from parsel import Selector
 import ast
 import create_csv_file
 
-search_term = "museum in paris"
+search_term = "Roofers in Lincoln Nebraska"
+# div_identifier must be looked up from a manual search 
+div_identifier = 'div.Nv2PK a.hfpxzc, div.tH5CWc a.hfpxzc, div.THOPZb a.hfpxzc' # "Roofers in Lincoln Nebraska"
 
 places = []
 
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()    
-    search_results = f'{google_maps_get_search_results_links.search("museum in paris", page=page)}'
+    search_results = f'{google_maps_get_search_results_links.search(search_term, page=page)}'
     search_results = ast.literal_eval(search_results)
     # print(search_results)
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     context = browser.new_context()
-    #add accept langauge header to display the HTML content in English
     context.set_extra_http_headers({
         "Accept-Language": "en-US,en;q=0.5",
         })
     page = context.new_page()
-    for url in search_results:        
+    for url in search_results:
         page.goto(url)
         page.wait_for_selector("//button[contains(@jsaction, 'reviewlegaldisclosure')]")
         places.append(google_maps_parse_single_place.parse_place(Selector(text=page.content())))
 
 print(json.dumps(places, indent=2, ensure_ascii=False))
 
-create_csv_file.dict_to_csv(places)
+create_csv_file.dict_to_csv(places, search_term)
